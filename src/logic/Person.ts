720d360f51659b49fb5e78454209f10d47f052d6
Ole Petersen <peteole2707@gmail.com>
2021-01-12 15:10:32 +0100

    fixed frequency error

--- src/logic/Person.ts
+++ src/logic/Person.ts
@@ -4,7 +4,7 @@ import { algorithmicConstants } from "./constants";
  * @typedef {(date:Date)=>{contactDensity:number,contactIntensity:number}} untrackedActivity
  */
 
-export interface UntrackedContact  {
+export interface UntrackedContact {
     /**-date at which the contact takes place */
     date: Date;
     /**person having an untracked contact */
@@ -17,10 +17,10 @@ export interface UntrackedContact  {
     everInfected: boolean;
 }
 /** generates next untracked contact starting at a given date */
-export type untrackedContactGenerator=(date:Date,person:Person)=>UntrackedContact;
+export type untrackedContactGenerator = (date: Date, person: Person) => UntrackedContact;
 
-const defaultContactGenerator:untrackedContactGenerator = (date: Date, person: Person) => ({
-    date: new Date(date.getTime() + 2 * person.untrackedFrequency * Math.random() * 1000*60*60*24),
+const defaultContactGenerator: untrackedContactGenerator = (date: Date, person: Person) => ({
+    date: new Date(date.getTime() + 2 * Math.random() * 1000 * 60 * 60 * 24 / person.untrackedFrequency),
     acuteInfected: 0.001 > Math.random(),
     everInfected: 0.01 > Math.random(),
     intensity: 2 * Math.random() * person.untrackedIntensity,
@@ -38,7 +38,7 @@ export class Person {
      * @param {string} name
      * @param {untrackedContactGenerator} externalActivity - generates next contact of person starting at given date
      */
-    constructor(name: string, untrackedFrequency = 1, untrackedIntensity = 0.1, externalActivity:untrackedContactGenerator = defaultContactGenerator) {
+    constructor(name: string, untrackedFrequency = 1, untrackedIntensity = 0.1, externalActivity: untrackedContactGenerator = defaultContactGenerator) {
         this.externalActivity = externalActivity;
         this.name = name;
         this.untrackedFrequency = untrackedFrequency;
